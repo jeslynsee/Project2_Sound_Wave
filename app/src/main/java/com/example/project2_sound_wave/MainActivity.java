5cd7a88908922a5e197c8e8d43faada38e309a3c
jeslynsee <jsee@csumb.edu>
2024-04-14 21:35:42 -0700

    Allowed user to sign up by clicking from Main Activity Created basic layout of Sign Up Page Wired up buttons of both Login and Sign Up

--- app/src/main/java/com/example/project2_sound_wave/MainActivity.java
+++ app/src/main/java/com/example/project2_sound_wave/MainActivity.java
@@ -2,13 +2,38 @@ package com.example.project2_sound_wave;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import com.example.project2_sound_wave.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
-
+    ActivityMainBinding binding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        binding.loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // message displaying success for now to indicate listener works for button
+                Toast.makeText(MainActivity.this, "Successfully logged in!", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        //if user clicks "Sign up here", it will redirect them to Sign Up page
+        binding.signUpRedirectTextView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // using intent here to start new activity AKA redirect user to sign up page/ start
+                // sign up activity
+                Intent intent = Sign_Up_Page.signUpIntentFactory(getApplicationContext());
+                startActivity(intent);
+            }
+        });
     }
 }
