f81844038804389e75f90f42b0ab89401e32aa00
Rigo <Rrayon@csumb.edu>
2024-04-17 13:20:03 -0700

    Started working on the home landing page. Added some genres need to add scroll. Added all genres need a submit button.

--- app/src/main/java/com/example/project2_sound_wave/MainActivity.java
+++ app/src/main/java/com/example/project2_sound_wave/MainActivity.java
@@ -1,141 +1,19 @@
 package com.example.project2_sound_wave;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.lifecycle.LiveData;
 
-import android.content.Context;
-import android.content.DialogInterface;
-import android.content.Intent;
-import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuInflater;
-import android.view.MenuItem;
-import android.view.View;
-import android.widget.Toast;
+import android.text.method.ScrollingMovementMethod;
 
-
-import com.example.project2_sound_wave.database.SoundWaveRepository;
-import com.example.project2_sound_wave.database.entities.User;
 import com.example.project2_sound_wave.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
-    private static final String MAIN_ACTIVITY_USER_ID = "com.example.project2_sound_wave.MAIN_ACTIVITY_USER_ID";
     ActivityMainBinding binding;
-
-    SoundWaveRepository repository;
-
-    private int loggedInUserId = -1;
-    private User user;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        binding = ActivityMainBinding.inflate(getLayoutInflater());
-        setContentView(binding.getRoot());
-
-        repository = SoundWaveRepository.getRepository(getApplication());
-
-        loginUser();
-//        invalidateOptionsMenu();
-
-
-        if (loggedInUserId == -1) {
-            Intent intent = Login_Page.loginIntentFactory(getApplicationContext());
-            startActivity(intent);
-        }
-
-        binding.SubmitButton.setOnClickListener(new View.OnClickListener() {
-        @Override
-            public void onClick(View v) {
-
-            }
-        });
-    }
-
-    private void loginUser() {
-        //TODO: Make loginUser FUNCTIONAL
-        loggedInUserId = user.getId();
-        if (loggedInUserId == -1) {
-            return;
-        }
-        LiveData<User> userObserver = repository.getUserByUserId(loggedInUserId);
-        userObserver.observe(this, user -> {
-            this.user = user;
-            if (this.user != null) {
-                invalidateOptionsMenu();
-            } else {
-                logout();
-            }
-        });
+        setContentView(R.layout.activity_main);
 
     }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        MenuInflater inflater = getMenuInflater();
-        inflater.inflate(R.menu.logout_menu, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onPrepareOptionsMenu(Menu menu) {
-        MenuItem item = menu.findItem(R.id.logoutMenuItem);
-        item.setVisible(true);
-        if (user == null) {
-            return false;
-        }
-        item.setTitle(user.getUsername());
-        item.setOnMenuItemClickListener(new MenuItem.OnMenuItemClickListener() {
-            @Override
-            public boolean onMenuItemClick(@NonNull MenuItem item) {
-                showLogOutDialog();
-                return false;
-            }
-        });
-        return super.onPrepareOptionsMenu(menu);
-    }
-
-    private void showLogOutDialog() {
-        AlertDialog.Builder alertBuilder = new AlertDialog.Builder(MainActivity.this);
-        final AlertDialog alertDialog = alertBuilder.create();
-
-        alertBuilder.setMessage("Are you sure you want to sign out?");
-        alertBuilder.setPositiveButton("Logout", new DialogInterface.OnClickListener() {
-            @Override
-            public void onClick(DialogInterface dialog, int which) {
-                logout();
-            }
-        });
-
-        alertBuilder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
-            @Override
-            public void onClick(DialogInterface dialog, int which) {
-                alertDialog.dismiss();
-            }
-        });
-
-        alertBuilder.create().show();
-
-    }
-
-    private void logout() {
-//        SharedPreferences sharedPreferences = getApplicationContext().getSharedPreferences(SHARED_PREFERENCE_USER_ID_KEY, Context.MODE_PRIVATE);
-//        SharedPreferences.Editor sharedPrefEditor = sharedPreferences.edit();
-//        sharedPrefEditor.putInt(SHARED_PREFERENCE_USER_ID_KEY, LOGGED_OUT);
-//        getIntent().putExtra(OPTIONS_PAGE_USER_ID, LOGGED_OUT);
-//        sharedPrefEditor.apply();
-
-        startActivity(Starting_Page.startingPageIntentFactory(getApplicationContext()));
-    }
-
-    static Intent mainActivityIntentFactory(Context context, int userId) {
-        Intent intent = new Intent(context, MainActivity.class);
-        intent.putExtra(MAIN_ACTIVITY_USER_ID, userId);
-        return intent;
-    }
-
-
 }
