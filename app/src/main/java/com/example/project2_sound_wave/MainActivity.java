941477822031910adb11fcbea66e1d1e1b524c23
Rigo <Rrayon@csumb.edu>
2024-04-17 13:20:03 -0700

    Started working on the home landing page. Added some genres need to add scroll. Added all genres need a submit button.

--- app/src/main/java/com/example/project2_sound_wave/MainActivity.java
+++ app/src/main/java/com/example/project2_sound_wave/MainActivity.java
@@ -3,12 +3,17 @@ package com.example.project2_sound_wave;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.method.ScrollingMovementMethod;
+
+import com.example.project2_sound_wave.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
+    ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
 }
