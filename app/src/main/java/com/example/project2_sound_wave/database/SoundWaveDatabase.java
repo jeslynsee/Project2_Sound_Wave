e074e9e5a8256fa2c65a2be7bfe7e511072aabe0
Rigo <Rrayon@csumb.edu>
2024-04-23 15:30:15 -0700

    Started working on the home landing page. Added some genres need to add scroll. Added all genres need a submit button. Added submit button needs to be linked to other page not done yet. About to merge.

--- app/src/main/java/com/example/project2_sound_wave/database/SoundWaveDatabase.java
+++ app/src/main/java/com/example/project2_sound_wave/database/SoundWaveDatabase.java
@@ -60,6 +60,7 @@ public abstract class SoundWaveDatabase extends RoomDatabase {
                 dao.insert(admin);
                 User testUser1 = new User("testuser1", "testuser1");
                 dao.insert(testUser1);
+
             });
         }
     };
