07304b3e799036c4eec388c972dafb49d824aca7
jeslynsee <jsee@csumb.edu>
2024-04-22 20:16:54 -0700

    Created Playlist table, entity, and DAO, but needs to be fully implemented. Need to create TypeConverters for List

--- app/src/main/java/com/example/project2_sound_wave/database/PlaylistDAO.java
+++ app/src/main/java/com/example/project2_sound_wave/database/PlaylistDAO.java
@@ -1,10 +1,12 @@
 package com.example.project2_sound_wave.database;
 
+import androidx.room.Dao;
 import androidx.room.Insert;
 import androidx.room.OnConflictStrategy;
 
 import com.example.project2_sound_wave.database.entities.Playlist;
 
+@Dao
 public interface PlaylistDAO {
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     void insert(Playlist ... playlists);
