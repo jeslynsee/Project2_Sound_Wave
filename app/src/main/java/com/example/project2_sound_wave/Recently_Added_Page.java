ee543085c5e9df2b68dd0944ce6bd30f76c83d32
jeslynsee <jsee@csumb.edu>
2024-04-23 18:35:49 -0700

    Created new activities to connect to main navigation; need to create Admin activities next. Worked on getting Sign Up page to be functional, need to fully implement this.

---
+++ app/src/main/java/com/example/project2_sound_wave/Recently_Added_Page.java
@@ -0,0 +1,24 @@
+package com.example.project2_sound_wave;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+
+import com.example.project2_sound_wave.databinding.ActivityRecentlyAddedPageBinding;
+
+public class Recently_Added_Page extends AppCompatActivity {
+    ActivityRecentlyAddedPageBinding binding;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityRecentlyAddedPageBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+    }
+
+    static Intent recentlyAddedPageIntentFactory(Context context) {
+        Intent intent = new Intent(context, Recently_Added_Page.class);
+        return intent;
+    }
+}
--- app/src/main/java/com/example/project2_sound_wave/database/PlaylistDAO.java
+++ app/src/main/java/com/example/project2_sound_wave/database/PlaylistDAO.java
@@ -1,15 +1,25 @@
 package com.example.project2_sound_wave.database;
 
+import androidx.lifecycle.LiveData;
 import androidx.room.Dao;
+import androidx.room.Delete;
 import androidx.room.Insert;
 import androidx.room.OnConflictStrategy;
+import androidx.room.Query;
 
 import com.example.project2_sound_wave.database.entities.Playlist;
+import com.example.project2_sound_wave.database.entities.User;
+
+import java.util.List;
 
 @Dao
 public interface PlaylistDAO {
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     void insert(Playlist ... playlists);
 
-    //TODO: Fully implement this
+    @Delete
+    void delete(Playlist playlist);
+
+    @Query("SELECT * FROM " + SoundWaveDatabase.USER_TABLE + " ORDER BY username")
+    LiveData<List<Playlist>> getAllPlaylists();
 }
